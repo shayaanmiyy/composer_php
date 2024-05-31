9ea21a42c5636bda8df5098a375988b524ff2a5a
Shayaan <shayaanmomin.sm@gmail.com>
2024-05-31 15:27:02 +0530

    Step 3 & Step 4

---
+++ src/Class1.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Class1;
+class MyClass
+{
+    public function hello()
+    {
+        echo "Hello India \n";
+    }
+
+    public function abcd()
+    {
+        echo "I am Alphabet \n";
+    }
+}
---
+++ src/Class2.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Class2;
+
+class MyClass
+{
+    public function hello()
+    {
+        echo "Hello DigitalPolygon \n";
+    }
+
+    public function pqrs()
+    {
+        echo "I am Alphabet PQRS \n";
+    }
+}
